cfb242a91b261c6531bcdfb41193e3bb01bd4553
D3vil0p3r <>
2023-12-11 22:02:40 +0100

    Removed dots.nix

--- dots.nix
+++
@@ -1,8 +0,0 @@
-{ home-manager, ... }:
-{
-  imports = [
-    home-manager.nixosModules.home-manager # to delete to avoid infinite recursion
-    ./home-manager/desktops/xfce
-  ];
-  athena.desktops.xfce.enable = true;
-}
