87377fad0d7086bb909790d8a158dd5230d215c4
D3vil0p3r <>
2023-12-26 03:47:30 +0100

    Update roles

--- home-manager/roles/web/default.nix
+++ home-manager/roles/web/default.nix
@@ -3,6 +3,7 @@
   home-manager.users.${username} = { pkgs, ...}: {
     home.packages = with pkgs; [
         aflplusplus
+        archivebox
         apachetomcatscanner
         arjun
         boofuzz
