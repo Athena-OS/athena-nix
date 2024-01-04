3827fbfac0b77d3cae228dcd5e3dd79602de0894
D3vil0p3r <>
2024-01-04 01:15:05 +0100

    Update package config

---
+++ pkgs/athena-config/package.nix
@@ -0,0 +1,31 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "athena-config";
+  version = "unstable-2024-01-04";
+
+  src = fetchFromGitHub {
+    owner = "Athena-OS";
+    repo = "athena-config";
+    rev = "1005952a365de3afb70d132e62ee404468ca9dfe";
+    hash = "sha256-7t4y20CbQEh0J5BgDUqwtcyMdPq0GvWQaXdzKft0+hA=";
+  };
+
+  installPhase = ''
+    mkdir -p $out/{bin,share}
+    cp -r bin/* $out/bin/
+    cp -r share/* $out/share/
+  '';
+
+  meta = with lib; {
+    description = "Athena OS environment files";
+    mainProgram = "athena-config";
+    homepage = "https://github.com/Athena-OS/athena-config";
+    maintainers = with maintainers; [ d3vil0p3r ];
+    platforms = platforms.unix;
+    license = licenses.gpl3;
+  };
+})
