65697a67f8abf8b710a3526feacd527c8f79815f
D3vil0p3r <vozaanthony@gmail.com>
2024-01-22 14:56:25 +0100

    Home Manager 23.11 in Flake

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
   
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
