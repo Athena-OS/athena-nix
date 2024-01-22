42087f1fc447e8d07a84327aa6287f7e6994039b
D3vil0p3r <vozaanthony@gmail.com>
2024-01-22 14:42:19 +0100

    Comment VMware declaration

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -54,10 +54,10 @@ in
 
     ];
 
-    virtualisation.vmware.host.enable = true;
+    /*virtualisation.vmware.host.enable = true;
     virtualisation.vmware.host.extraConfig = ''
       # Allow unsupported device's OpenGL and Vulkan acceleration for guest vGPU
       mks.gl.allowUnsupportedDrivers = "TRUE"
       mks.vk.allowUnsupportedDevices = "TRUE"
-    '';
+    '';*/
 }
