f36ee39a5062ad8200e908ac4c6e42a042c1cf6f
D3vil0p3r <vozaanthony@gmail.com>
2024-01-22 06:09:03 +0100

    Fix issues

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -58,6 +58,6 @@ in
     virtualisation.vmware.host.extraConfig = ''
       # Allow unsupported device's OpenGL and Vulkan acceleration for guest vGPU
       mks.gl.allowUnsupportedDrivers = "TRUE"
-      mks.vk.allowUnsupportedDevices = "TRUE"*/
-    '';
+      mks.vk.allowUnsupportedDevices = "TRUE"
+    '';*/
 }
