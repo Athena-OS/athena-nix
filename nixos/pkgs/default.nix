f603f13346decd38eda1d75a55e817466c5f631a
D3vil0p3r <crazysnob@live.it>
2024-02-26 13:23:35 +0100

    Update default.nix

--- nixos/pkgs/default.nix
+++ nixos/pkgs/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    (callPackage ./athena-config/package.nix { })
+    (callPackage ./athena-config-nix/package.nix { })
     (callPackage ./athena-welcome/package.nix { })
     (callPackage ./cyber-toolnix/package.nix { })
     (callPackage ./nist-feed/package.nix { })
