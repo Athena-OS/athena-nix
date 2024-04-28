84466835ae963c4dde8648e82531a287b14a2d8f
D3vil0p3r <crazysnob@live.it>
2024-04-28 21:12:52 +0200

    Update default.nix

--- nixos/home-manager/browsers/firefox/default.nix
+++ nixos/home-manager/browsers/firefox/default.nix
@@ -5,7 +5,7 @@
     programs.firefox = {
       enable = true;
       # package = pkgs.firefox.override {cfg.enableTridactylNative = true;};
-      package = pkgs.firefox;
+      package = pkgs.firefox-esr;
       
       profiles = {
         "${username}" = {
