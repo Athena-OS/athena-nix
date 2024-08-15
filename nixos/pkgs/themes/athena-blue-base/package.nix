c0be4bf56d7e8e81c15068b6fbf986c4ede6e1b6
D3vil0p3r <vozaanthony@gmail.com>
2024-08-15 16:05:46 +0200

    Update color base packages

--- nixos/pkgs/themes/athena-blue-base/package.nix
+++ nixos/pkgs/themes/athena-blue-base/package.nix
@@ -6,20 +6,28 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "athena-blue-base";
-  version = "0-unstable-2024-04-25";
+  version = "0-unstable-2024-08-15";
 
   src = fetchFromGitHub {
     owner = "Athena-OS";
     repo = "athena-blue-base";
-    rev = "f0741badc21bbc52eb06c19944179b91a71fa437";
-    hash = "sha256-kqPR9LfrBGsP4SF64D43cr8oHOpFeFG2v+vXm1ShMmA=";
+    rev = "ea5a4b91729ab68c4a31b1ec8e676e39f0774079";
+    hash = "sha256-q/DMUUsnkOdEMLjY4priEvuifyXoW5beC5sWPObEm1I=";
   };
 
+  postPatch = ''
+    substituteInPlace gnome-background-properties/*.xml \
+      --replace-fail /usr/share/backgrounds $out/share/backgrounds
+  '';
+
   installPhase = ''
-    mkdir -p $out/share/{icons/hicolor/scalable/{apps,categories},backgrounds/athena}
+    mkdir -p $out/share/{icons/hicolor/scalable/{apps,categories},backgrounds/athena,gnome-background-properties}
     cp -r icons/apps/* $out/share/icons/hicolor/scalable/apps/
     cp -r icons/categories/* $out/share/icons/hicolor/scalable/categories/
-    cp -r nix-behind.png $out/share/backgrounds/athena/
+    cp -r wallpapers/arch-ascii.png $out/share/backgrounds/athena/
+    cp -r wallpapers/nix-behind.png $out/share/backgrounds/athena/
+    cp -r gnome-background-properties/arch-ascii.xml $out/share/gnome-background-properties/
+    cp -r gnome-background-properties/nix-behind.xml $out/share/gnome-background-properties/
   '';
 
   meta = with lib; {
