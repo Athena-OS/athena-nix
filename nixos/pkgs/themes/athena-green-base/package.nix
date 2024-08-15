a82f6056f7a635202a0f29f34188626881806a8a
D3vil0p3r <vozaanthony@gmail.com>
2024-08-15 16:05:46 +0200

    Update color base packages

--- nixos/pkgs/themes/athena-green-base/package.nix
+++ nixos/pkgs/themes/athena-green-base/package.nix
@@ -6,21 +6,28 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "athena-green-base";
-  version = "0-unstable-2024-07-13";
+  version = "0-unstable-2024-08-15";
 
   src = fetchFromGitHub {
     owner = "Athena-OS";
     repo = "athena-green-base";
-    rev = "2d74a76c6ac42efe3bbf20f12d7a3e3595618bfe";
-    hash = "sha256-pBoX5rTPaXlCFojlfFhE37l4u9QMWQHBJAKEAyMTFko=";
+    rev = "e50454fa38a6d75905a30093a5ef438114f76e55";
+    hash = "sha256-qU5QbzmY89BMCrVegP/2jxITRvLXXqTI0f7/5N8yRvo=";
   };
 
+  postPatch = ''
+    substituteInPlace gnome-background-properties/*.xml \
+      --replace-fail /usr/share/backgrounds $out/share/backgrounds
+  '';
+
   installPhase = ''
-    mkdir -p $out/share/{icons/hicolor/scalable/{apps,categories},backgrounds/athena}
+    mkdir -p $out/share/{icons/hicolor/scalable/{apps,categories},backgrounds/athena,gnome-background-properties}
     cp -r icons/apps/* $out/share/icons/hicolor/scalable/apps/
     cp -r icons/categories/* $out/share/icons/hicolor/scalable/categories/
-    cp -r hackthebox.png $out/share/backgrounds/athena/
-    cp -r nix-hackthebox.png $out/share/backgrounds/athena/
+    cp -r wallpapers/hackthebox.png $out/share/backgrounds/athena/
+    cp -r wallpapers/nix-hackthebox.png $out/share/backgrounds/athena/
+    cp -r gnome-background-properties/hackthebox.xml $out/share/gnome-background-properties/
+    cp -r gnome-background-properties/nix-hackthebox.xml $out/share/gnome-background-properties/
   '';
 
   meta = with lib; {
