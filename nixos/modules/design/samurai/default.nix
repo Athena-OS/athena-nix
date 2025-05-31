335d072f541595b9bbe1378697047073d115769c
Antonio <crazysnob@live.it>
2025-06-01 00:34:40 +0200

    Update default.nix

--- nixos/modules/design/samurai/default.nix
+++ nixos/modules/design/samurai/default.nix
@@ -27,6 +27,7 @@ in {
           name = gtkTheme;
           package = pkgs.tokyonight-gtk-theme.override {
             colorVariants = [ "dark" ];
+            tweakVariants = [ "macos" ];
           };
         };
 
