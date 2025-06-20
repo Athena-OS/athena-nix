750e89144fc6334eee3507dfc4e72fc9cd0c0fa9
D3vil0p3r <vozaanthony@gmail.com>
2025-06-21 01:37:49 +0200

    Update files

--- nixos/home-manager/shells/fish/default.nix
+++ nixos/home-manager/shells/fish/default.nix
@@ -85,18 +85,6 @@
 
           set -g fish_color_autosuggestion 848cb5
 
-          #set -x BFETCH_INFO "pfetch"
-          #set -x BFETCH_ART "$HOME/.local/textart/fetch/unix.textart"
-          #set -x PF_INFO "Unix Genius"
-
-          #set -x BFETCH_INFO "curl --silent --location 'wttr.in/rome?0pq'"
-          #set -x BFETCH_ART "printf \"\033[35m\"; figlet -f Bloody Spooky"
-          #set -x BFETCH_COLOR "$HOME/.local/textart/color/icon/ghosts.textart"
-
-          #set -x BFETCH_INFO "exa -la"
-          #set -x BFETCH_ART "$HOME/.local/textart/fetch/pacman-maze.textart"
-          #set -x BFETCH_COLOR "$HOME/.local/textart/color/icon/pacman.textart"
-
           set -x BFETCH_INFO "pfetch"
           set -x BFETCH_ART "cowsay '<3 Athena OS'"
           set -x BFETCH_COLOR "$HOME/.local/textart/color/icon/panes.textart"
@@ -120,7 +108,6 @@
           set -x WEBCOMMON "$SECLISTS/Discovery/Web-Content/common.txt"
           set -x WEBPARAM "$SECLISTS/Discovery/Web-Content/burp-parameter-names.txt"
 
-          set -gx TERM xterm-256color
           if status is-interactive
               # Commands to run in interactive sessions can go here
               if not set -q NO_REPETITION
