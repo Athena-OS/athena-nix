c9913d4b5f11aab0d2b3d1fded68a3c96a1c9820
Antonio <crazysnob@live.it>
2025-06-25 09:47:53 +0200

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -5,7 +5,7 @@
 { lib, config, pkgs, ... }:
 let
   # These variable names are used by Aegis backend
-  version = "25.05"; #unstable or 25.05
+  version = "unstable"; #unstable or 25.05
   username = "athena";
   hashed = "$6$zjvJDfGSC93t8SIW$AHhNB.vDDPMoiZEG3Mv6UYvgUY6eya2UY5E2XA1lF7mOg6nHXUaaBmJYAMMQhvQcA54HJSLdkJ/zdy8UKX3xL1";
   hashedRoot = "$6$zjvJDfGSC93t8SIW$AHhNB.vDDPMoiZEG3Mv6UYvgUY6eya2UY5E2XA1lF7mOg6nHXUaaBmJYAMMQhvQcA54HJSLdkJ/zdy8UKX3xL1";
