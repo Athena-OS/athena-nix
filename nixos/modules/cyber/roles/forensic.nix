786723338a6777fc9ed3594ea22de4a81b04fe48
Antonio <crazysnob@live.it>
2025-07-15 15:22:14 +0200

    Update forensic.nix

--- nixos/modules/cyber/roles/forensic.nix
+++ nixos/modules/cyber/roles/forensic.nix
@@ -3,10 +3,11 @@
 with pkgs;
 
 [
-  #acquire
+  acquire
   aesfix
   aeskeyfind
   afflib
+  autopsy
   bmap-tools
   bulk_extractor
   chainsaw
@@ -14,7 +15,7 @@ with pkgs;
   chkrootkit
   chntpw
   dc3dd
-  #dcfldd
+  dcfldd
   ddrescue
   dmg2img
   exiftool
@@ -41,10 +42,10 @@ with pkgs;
   safecopy
   scalpel
   sleuthkit
-  #snort
+  snort
   tell-me-your-secrets
   testdisk
-  #tracee
+  tracee
   usbrip
   volatility3
 ]
